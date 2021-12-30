abae749cde0581c841d2c5de059724a5ad307d12
PWilFormation <petit.wilfrid.formation@gmail.com>
2021-12-30 16:06:16 +0100

    Création de la classe Operation avec ses attributs, constructeurs et propriétés.

--- TpCompteBancaire/Classes/Operation.cs
+++ TpCompteBancaire/Classes/Operation.cs
@@ -8,5 +8,24 @@ namespace TpCompteBancaire.Classes
 {
     internal class Operation
     {
+        private string date;
+        private string heure;
+        private double montant;
+
+        public Operation()
+        {
+
+        }
+
+        public Operation(string date, string heure, double montant)
+        {
+            Date = date;
+            Heure = heure;
+            Montant = montant;
+        }
+
+        public string Date { get => date; set => date = value; }
+        public string Heure { get => heure; set => heure = value; }
+        public double Montant { get => montant; set => montant = value; }
     }
 }
