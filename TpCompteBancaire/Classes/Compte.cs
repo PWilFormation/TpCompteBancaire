27a084d7c7f843f13d1ab46a8176992187884f22
PWilFormation <petit.wilfrid.formation@gmail.com>
2021-12-30 15:59:32 +0100

    Création de la classe Compte avec ses attributs, constructeurs et propriétés. Et initialisation de ses attributs.

--- TpCompteBancaire/Classes/Compte.cs
+++ TpCompteBancaire/Classes/Compte.cs
@@ -8,5 +8,20 @@ namespace TpCompteBancaire.Classes
 {
     internal class Compte
     {
+        private static int id_compte = 0;
+        private double solde = 0;
+
+        public Compte()
+        {
+            id_compte++;
+        }
+
+        public Compte(double solde) : this()
+        {
+            Solde = solde;
+        }
+
+
+        public double Solde { get => solde; set => solde = value; }
     }
 }
