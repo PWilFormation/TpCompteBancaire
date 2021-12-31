c695197d60a5a7c868b6687f88c1fe670fc9caa3
ybraidiz <95235040+ybraidiz@users.noreply.github.com>
2021-12-31 19:12:59 +0100

    Add files via upload

---
+++ Compte.cs
@@ -0,0 +1,208 @@
+﻿using System;
+namespace TpListCompeBancaire
+{
+    public class Compte
+    {
+        string Nom;
+        string Prenom;
+        double NumTelephone;
+        double Solde;
+        double Code;
+        public Compte()
+        {
+            
+
+        }
+        public Compte(string nom, string prenom, double numTelephone, double solde,double code)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            NumTelephone = numTelephone;
+            Solde = solde;
+            Code = code;
+
+        }
+        public string nom { get => Nom; set => Nom = value; }
+        public string prenom { get => Prenom; set => Prenom = value; }
+        public double numTelephone { get => NumTelephone; set => NumTelephone = value; }
+        public double solde { get => Solde; set => Solde = value; }
+        public double code { get => Code; set => Code = value; }
+
+        public void CreationDeCompte(double solde, double code)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Compte bien crée, voici le numéro de compte : {code}");
+            Console.WriteLine($"Votre compte à un solde de {solde}.");
+            Console.ResetColor();
+        }
+
+        public void Information(string Nom, string Prenom, double NumTelephone, double solde, double retrait)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Nom : {Nom}, Prénom : {Prenom}, Téléphone : {NumTelephone}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Solde : {solde}Euro");
+            Console.ResetColor();
+        }
+
+    }
+
+    public class Operation : Compte
+    {
+        double Solde;
+        double Retrait;
+        public Operation()
+        {
+
+        }
+       
+        public double DepotDeLargent(double code)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"***** Déposer de l'argent *****\n");
+            Operation Test = new Operation();
+            bool bol = Test.VerifierLeCode(code);
+            if (bol == true)
+            {
+                Console.Write("Merci de saisir le montant du depot : ");
+                if (double.TryParse(Console.ReadLine(), out Solde))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Dépot reussi");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Erreur de saisie, Merci de saisir un nombre");
+                    solde = Convert.ToDouble(Console.ReadLine());
+                    Console.ResetColor();
+
+                }
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Erreur de saisie, Merci de saisir le code");
+                double Testcode = Convert.ToDouble(Console.ReadLine());
+                Console.ResetColor();
+
+            }
+
+            return Solde;
+        }
+
+        public double RetraitDeLargent(double code, double solde)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***** Retirer de l'argent *****\n ");
+            Operation Test = new Operation();
+            bool bol = Test.VerifierLeCode(code);
+            if (bol == true)
+            {
+                Console.Write("Merci de saisir le montant du Retrait : ");
+                if (double.TryParse(Console.ReadLine(), out Retrait))
+                {
+                    if (Retrait <= solde)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Retrait reussi");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Votre solde de {solde}Euro est insuffisant pour effectuer cette opération.");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\tErreur de saisie, Merci de saisir un nombre");
+                    Retrait = Convert.ToDouble(Console.ReadLine());
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\tErreur de saisie, Merci de saisir Le Code : ");
+                code = Convert.ToDouble(Console.ReadLine());
+                Console.ResetColor();
+            }
+            
+            return Retrait;
+            
+        }
+
+        public bool VerifierLeCode(double code)
+        {
+            bool bol;
+            Console.Write("Merci de saisir le numero de compte : ");
+            if (double.TryParse(Console.ReadLine(), out double Testcode))
+            {
+                if (Testcode == code)
+                {
+                    bol = true;
+                }
+                else
+                {
+                    bol = false;
+                }
+            }
+            else
+            {
+                bol = false;
+            }
+
+            return bol;
+        }
+        public void AffichageDuCompte(string nom, string prenom, double numDeTelephone, double solde, double code, double retrait)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n***** Afficher un compte *****\n ");
+            Operation Test = new Operation();
+            bool bol = Test.VerifierLeCode(code);
+            if (bol ==true)
+            {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Liste Opérations : ");
+                    Compte cmp = new Compte();
+                    cmp.Information(nom, prenom, numDeTelephone, solde,retrait);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\tErreur de saisie, Merci de saisir le Code");
+                double TestCode = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        public class Utilisateur : Compte
+        {
+            
+            double NumDeTelephone;
+            double Solde;
+            public Utilisateur()
+            {
+               
+
+            }
+            public Utilisateur(string Nom, string Prenom, double NumTelephone, double solde)
+            {
+                nom = Nom;
+                prenom = Prenom;
+                NumDeTelephone = numTelephone;
+                Solde = solde;
+
+            }
+
+            
+        }
+
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpListCompeBancaire
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            double retrait = 0;
+            double solde = 0;
+            string nom="";
+            string prenom="";
+            double numDeTelephone=0;
+            
+            int choix;
+
+            Compte cmp = new Compte();
+            Random test = new Random();
+            Operation cmm = new Operation();
+            int code = test.Next(9999, 99999);
+
+            do
+            {
+                
+                Console.WriteLine("\n****** Banque Peu Populaire ******");
+                Console.WriteLine("1-Créer un compte bancaire");
+                Console.WriteLine("2-Déposer de l'argent");
+                Console.WriteLine("3-Retirer de l'argent ");
+                Console.WriteLine("4-Afficher un compte bancaire ");
+                Console.Write("Entrer votre choix : ");
+                if (int.TryParse(Console.ReadLine(), out choix))
+                {
+
+                    switch (choix)
+                    {
+                        case int tmp when (tmp == 1):
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"\n***** Création d'un nouveau compte *****\n");
+                            Console.Write("Le nom du client : ");
+                            nom = Convert.ToString(Console.ReadLine());
+                            Console.Write("\nLe prénom du client : ");
+                            prenom = Convert.ToString(Console.ReadLine());
+                            Console.Write("\nLe téléphone du client : ");
+                            numDeTelephone = Convert.ToDouble(Console.ReadLine());
+                            Compte comp = new Compte();
+                            Console.Clear();
+                            comp.CreationDeCompte(solde, code);
+                            
+                            break;
+                        case int tmp when (tmp == 2):
+                            solde = cmm.DepotDeLargent(code);
+                            break;
+                        case int tmp when (tmp == 3):
+                            retrait = cmm.RetraitDeLargent(code, solde);
+                            solde = solde - retrait;
+                            break;
+                        case int tmp when (tmp == 4):
+                            cmm.AffichageDuCompte(nom, prenom, numDeTelephone, solde, code, retrait);
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Erreure de saisie.\n ");
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\tErreur de saisie, Merci de saisir un chiffre entre 0 et 4!");
+                    choix = Convert.ToInt32(Console.ReadLine());
+                    continue;
+
+                }
+
+
+            } while (choix != 0);
+        }
+    }
+}
